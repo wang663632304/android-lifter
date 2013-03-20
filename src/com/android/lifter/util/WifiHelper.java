0cb2036536b0bbeefd067b1336c3f133dbd55233
Matt Rutkowski <matrut@gmail.com>
2013-03-20 21:42:20 +0100

    WiFi stuff

--- src/com/android/lifter/util/WifiHelper.java
+++ src/com/android/lifter/util/WifiHelper.java
@@ -1,5 +1,15 @@
 package com.android.lifter.util;
 
-public class WifiHelper {
+import android.content.Context;
+import android.net.ConnectivityManager;
 
+public class WifiHelper 
+{
+	public boolean isWifi(Context ctx)
+    {
+        ConnectivityManager hConnMngr = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean bInetWifi = hConnMngr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
+        return bInetWifi;
+    }
+	
 }
