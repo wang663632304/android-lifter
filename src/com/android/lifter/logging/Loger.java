12829277d068f28ce57a980a6a6dbd6e3ee8256b
Matt Rutkowski <matrut@gmail.com>
2013-03-29 14:10:47 +0100

    Adding time to Loger

--- src/com/android/lifter/logging/Loger.java
+++ src/com/android/lifter/logging/Loger.java
@@ -1,5 +1,8 @@
 package com.android.lifter.logging;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import android.util.Log;
 
 public class Loger 
@@ -11,14 +14,16 @@ public class Loger
 		String szClass = Thread.currentThread().getStackTrace()[3].getClassName() + ":" + Thread.currentThread().getStackTrace()[3].getMethodName() + "()";
 		String szLine = "" + Thread.currentThread().getStackTrace()[3].getLineNumber();
 		
-		Log.d("@@@","-----------------------------");
-		Log.d("@@@","-----------------------------");
+		
+		Log.d("@@@","-------------------------------------------------------------------------------------");
+		Log.d("@@@","-------------------------------------------------------------------------------------");
 		Log.d("@@@","file:  " + szFile);
 		Log.d("@@@","line:  " + szLine);
 		Log.d("@@@","class: " + szClass);
-		Log.d("@@@","-----------------------------");
+		Log.d("@@@","time:  " + new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime()));
+		Log.d("@@@","-------------------------------------------------------------------------------------");
 		Log.d("@@@",szText.trim());
-		Log.d("@@@","-----------------------------");
-		Log.d("@@@","-----------------------------");
+		Log.d("@@@","-------------------------------------------------------------------------------------");
+		Log.d("@@@","-------------------------------------------------------------------------------------");
 	}
 }
