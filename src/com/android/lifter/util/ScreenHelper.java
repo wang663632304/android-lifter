38a86a4ee907327be52f19570da2a1f9d6352845
Matt Rutkowski <matrut@gmail.com>
2013-04-03 12:16:01 +0200

    Adding screen size calculator to ScreenHelper

--- src/com/android/lifter/util/ScreenHelper.java
+++ src/com/android/lifter/util/ScreenHelper.java
@@ -1,6 +1,7 @@
 package com.android.lifter.util;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 
 public class ScreenHelper 
 {
@@ -12,5 +13,15 @@ public class ScreenHelper
 	public static int getScreenWidth(Context ctx)
 	{
 		return ctx.getResources().getDisplayMetrics().widthPixels;
+	}
+
+	public static int getScreeenInches(Context ctx)
+	{
+		 DisplayMetrics dm = ctx.getResources().getDisplayMetrics();
+
+         float screenWidth  = dm.widthPixels / dm.xdpi;
+         float screenHeight = dm.heightPixels / dm.ydpi;
+
+         return (int) Math.sqrt(Math.pow(screenWidth, 2) + Math.pow(screenHeight, 2));
 	}
 }
